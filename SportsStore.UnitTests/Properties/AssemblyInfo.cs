61aafc852a56a559b585e6353845cfc7fa4aaa26
Rodion Horbulenko <Rodion.Gorbulenko@gmail.com>
2018-05-27 15:49:55 +0300

    Add project files.

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("579b648f-f199-4d2c-a1f6-620d4e776b39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/Infrastructure/UnityDependencyResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Unity;
+using Unity.Extension;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class UnityDependencyResolver
+    {
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            
+        }
+    }
+}
