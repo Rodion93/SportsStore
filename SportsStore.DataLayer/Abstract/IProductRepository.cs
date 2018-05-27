5a0f20fa0bd23db6854a3ac841db46806b99a163
Rodion Horbulenko <Rodion.Gorbulenko@gmail.com>
2018-05-27 16:01:54 +0300

    Mock repository

--- SportsStore.DataLayer/Abstract/IProductRepository.cs
+++ SportsStore.DataLayer/Abstract/IProductRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SportsStore.DataLayer.Entities;
 
 namespace SportsStore.DataLayer.Abstract
 {
-    interface IProductRepository
+    public interface IProductRepository
     {
+        IEnumerable<Product> Products { get; }
     }
 }
